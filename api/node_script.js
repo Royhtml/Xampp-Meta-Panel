cc87788cd728322388e128f3b57802547573827f
Wolfes <dwigames17@gmail.com>
2026-05-14 16:57:35 +0700

    update

--- api/node_script.js
+++
@@ -1,9 +0,0 @@
-// api/node_script.js
-export default function handler(req, res) {
-  const dataDariNode = {
-    status: "sukses",
-    pesan: "Ini adalah hasil eksekusi dari Node.js",
-    waktu: new Date().toISOString()
-  };
-  res.status(200).json(dataDariNode);
-}
